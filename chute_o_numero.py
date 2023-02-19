263d3058043bf20ae2c5faa5dfcd3f8eee086cb1
Caio Freire Lima de Assis <CS376562@MinhaTI.com.br>
2023-02-19 00:27:43 -0300

    feat(chute_o_numero):Adicionando script que gera um numero e pede para usuario adivinhar.

---
+++ chute_o_numero.py
@@ -0,0 +1,32 @@
+import random
+
+class ChuteONumero:
+  def __init__(self):
+    self.valor_aleatorio: 0
+    self.valor_minimo = 1
+    self.valor_maximo = 100
+    self.tentar_novamente = True
+
+  def iniciar(self):
+    self.GerarNumeroAleatorio()
+    self.PedirValorAleatorio()
+    while self.tentar_novamente == True:
+      if int(self.valor_do_chute) > self.valor_aleatorio:
+        print('Chute um valor mais baixo!')
+        self.PedirValorAleatorio()
+      elif int(self.valor_do_chute) < self.valor_aleatorio:
+        print('Chute um valor mais alto!')
+        self.PedirValorAleatorio()
+      if int(self.valor_do_chute) == self.valor_aleatorio:
+        self.tentar_novamente = False
+      print('Parabens você acertou!!')
+
+  def PedirValorAleatorio(self):
+    self.valor_do_chute = input('Chute um número: ')
+    
+  def GerarNumeroAleatorio(self):
+    self.valor_aleatorio =  random.randint(self.valor_minimo,self.valor_maximo)
+    print(self.valor_aleatorio)
+    
+chute = ChuteONumero()
+chute.iniciar()
